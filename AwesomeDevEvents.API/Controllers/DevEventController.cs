1ac4e48a5475680a05a6fe3eba103c8331e94295
Manoel Victor Silva Lira <manoelvictorzxc17@hotmail.com>
2023-11-14 12:18:27 -0300

    Adicionar arquivos de projeto.

---
+++ AwesomeDevEvents.API/Controllers/DevEventController.cs
@@ -0,0 +1,65 @@
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventController : ControllerBase
+    {
+        private readonly DevEventsDbContext _dbContext;
+
+        public DevEventController(DevEventsDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var events = _dbContext.DevEvents.Where(e => !e.IsDeleted).ToList();
+            return Ok(events);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(Guid id)
+        {
+            var devEvent = _dbContext.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+                return NotFound();
+
+            return Ok(devEvent);
+        }
+
+        [HttpPost]
+        public ActionResult Post(DevEvent devEvent)
+        {
+            _dbContext.DevEvents.Add(devEvent);
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(Guid id, DevEvent input)
+        {
+            var devEvent = _dbContext.DevEvents.SingleOrDefault(d => d.Id == id);
+            if (devEvent == null)
+                return NotFound();
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var devEvent = _dbContext.DevEvents.SingleOrDefault(d => d.Id == id);
+            if (devEvent == null)
+                return NotFound();
+
+            devEvent.Delete();
+            return NoContent();
+        }
+    }
+}
